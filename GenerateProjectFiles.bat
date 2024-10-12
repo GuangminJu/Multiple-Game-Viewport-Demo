ecc0e1d57d9e01ea92231d2ad2791f9e04304d00
Mellos <1429326774@qq.com>
2024-10-12 20:43:15 +0800

    Fixed Bug

--- GenerateProjectFiles.bat
+++ GenerateProjectFiles.bat
@@ -8,14 +8,14 @@ rem rmdir /s /q Saved
 rmdir /s /q DerivedDataCache
 
 
-set MyUVS="E:\UnrealEngine-5.3\Engine\Binaries\Win64\UnrealVersionSelector-Win64-Shipping.exe"
-set MyUBT="E:\UnrealEngine-5.3\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe"
+set MyUVS="E:\UnrealEngineBinaries\UE_5.4\Engine\Binaries\Win64\UnrealVersionSelector-Win64-Shipping.exe"
+set MyUBT="E:\UnrealEngineBinaries\UE_5.4\Engine\Binaries\DotNET\UnrealBuildTool\UnrealBuildTool.exe"
 
 rem change Transformation to your own project name
 set MyFullPath="%cd%\CGVDemo"
 
 
-%MyUVS% /projectfiles %MyFullPath%.uproject
+%MyUVS% /projectfiles %MyFullPath%.uproject -game
 
 %MyUBT% Development Win64 -Project=%MyFullPath%.uproject -TargetType=Editor -Progress -NoHotReloadFromIDE
 
--- Source/CGVDemo/CGVDemo.Build.cs
+++ Source/CGVDemo/CGVDemo.Build.cs
@@ -7,6 +7,7 @@ public class CGVDemo : ModuleRules
 	public CGVDemo(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		PublicDependencyModuleNames.AddRange(new string[]
 			{ "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay"});
