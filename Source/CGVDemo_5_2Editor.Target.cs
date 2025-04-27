7e5d8cdaab0f8db60dda852fbdb86d300fb45f3a
Mellos <1429326774@qq.com>
2025-04-28 02:03:39 +0800

    5.4

--- Source/CGVDemo_5_2Editor.Target.cs
+++ Source/CGVDemo_5_2Editor.Target.cs
@@ -8,8 +8,8 @@ public class CGVDemo_5_2EditorTarget : TargetRules
 	public CGVDemo_5_2EditorTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V2;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("CGVDemo_5_2");
 	}
 }
